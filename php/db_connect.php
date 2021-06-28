16283c44c778c546bf65133149def8576bec94c5
ivanstud <kachur140701@gmail.com>
2021-06-28 02:14:27 +0200

    Created

---
+++ php/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+    $db_url = "localhost";
+    $db_user = "root";
+    $db_password = "";
+    $db_name = "todolist";
+
+    $connection = mysqli_connect($db_url, $db_user, $db_password, $db_name);
+    
+    if(!$connection){
+        echo "<div class='alert alert-danger'>";
+        die("Database connection failed!");
+        echo "</div>";
+    }
+?>
